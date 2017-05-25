a877bd8b5d164ae66d547581dedb6ec5e35bdab8
AdamSkuse <a.t.skuse@gmail.com>
2017-05-24 17:11:42 -0700

    Installs ESLint successfully

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function doGood() {
+    var anotherMessage = 'doing good!';
+    var message = 'or am i?';
+
+    console.log('doing something');
+    console.log(message + anotherMessage);
+
+    var toDoList = ['List','things','to do'];
+    console.log(toDoList);
+}
+
+doGood();
+
